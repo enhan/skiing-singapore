69b5bcbb06c89970ca9746c7b69715d102e68d4b
Emmanuel Nhan <nhanmanu@gmail.com>
2015-08-09 19:58:08 +0200

    Added some profiling, juste to compare with the stream based impl

--- src/main/scala/eu/enhan/skiing/SkiingApp.scala
+++ src/main/scala/eu/enhan/skiing/SkiingApp.scala
@@ -2,6 +2,7 @@ package eu.enhan.skiing
 
 import dispatch._
 import org.slf4j.LoggerFactory
+import org.slf4j.profiler.Profiler
 import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
@@ -11,13 +12,17 @@ object SkiingApp extends App{
   val log = LoggerFactory.getLogger("app")
 
   val parser = new MapParser
-
+  val profiler = new Profiler("Downloader")
+  profiler.setLogger(log)
+  profiler.start("Download & parse")
   val svc = url("http://s3-ap-southeast-1.amazonaws.com/geeks.redmart.com/coding-problems/map.txt")
   Http(svc OK as.String).map{ data =>
     val rawMap = parser.parse(data)
+    profiler.stop().print()
     val skiMap = new SkiMap(rawMap)
     log.info("Starting computation")
     val path = skiMap.longestAndSteepestPath
+
     log.info("Computation done")
     log.info(s"Longest and steepest path is ${path.length} long and drops from ${path.startHeight} to ${path.endHeight} for a drop of ${path.drop}")
     Http.shutdown()
